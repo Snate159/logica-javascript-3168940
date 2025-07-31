9c240e372b1bc13458c5d73916c8d83feef27f93
Snate159 <santi.rrplay@gmail.com>
2025-07-31 13:27:31 -0500

    Ejercicio terminado

--- 2_operadores_comparacion_logicos/app.js
+++ 2_operadores_comparacion_logicos/app.js
@@ -13,14 +13,22 @@ let num2 = Number(prompt("Dame un segundo numero"))
 console.log("Su valor A es: " + num1)
 console.log("Su valor B es: " + num2)
 
-suma = num1 + num2
-console.log("Suma: " + suma)
+let suma = num1 + num2
+let resta = num1 - num2
+let multiplicacion = num1 * num2
+let division = num1 / num2
 
-resta = num1 - num2
-console.log("Resta: " + resta)
+/* let suma = num1 + num2
+console.log("El valor de la Suma del numero " + num1 +  " y el numero " + num2 + " es: " + suma)
 
-multiplicacion = num1 * num2
-console.log("Multiplicacion: " + multiplicacion)
+let resta = num1 - num2
+console.log("El valor de la Resta del numero " + num1 + " y el numero " + num2 + " es: " + resta)
 
-division = num1 / num2
-console.log("Division: " + division)
+let multiplicacion = num1 * num2
+console.log("El valor de la Multiplicacion del numero " + num1 + " y el numero " + num2 + " es: " + multiplicacion)
+
+let division = num1 / num2
+console.log("El valor de la Division del numero " + num1 + " y el numero " + num2 + " es: " + division) */
+
+//
+console.log(`Tu numero es ${num1}`)
