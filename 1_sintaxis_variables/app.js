38aab62bf336b485ab5438169e0dd9902bc0fe69
Snate159 <santi.rrplay@gmail.com>
2025-07-31 12:26:04 -0500

    Repositorio js version 1

---
+++ 1_sintaxis_variables/app.js
@@ -0,0 +1,26 @@
+// el alert muestra una ventana emergente en el navegador
+//alert("Hola Mundo")
+// el prompt muestra una caja para ingresar informacion desde el navegador
+//prompt("Dime tu Nombre")
+
+// variable declarada con let
+let personaje = "Perrito"
+// crear variabe puntaje y asignar valor de 0
+// los valores numericos van sin comillas
+let puntaje = 0
+//Crear variable shots y asignar valor de 3
+let shots = 3
+    recarga = 2
+
+    console.log("Total balas", shots + recarga)
+
+    // el usuario capturo 4 patos
+puntaje += 4
+    //el usuario gasto 2 balas
+shots -= 2
+    //el usuario recaargo su arma
+recarga +=1
+
+console.log ("Puntaje:", puntaje)
+console.log ("Shots:", shots)
+console.log ("Recarga:", recarga)
---
+++ 2_operadores_comparacion_logicos/app.js
@@ -0,0 +1,26 @@
+//Boolean(...)
+
+/*console.log(' "5 == "5" ', "5" == 5" ) //true
+console.log( "5" === 5", "5" === 5" ) //true 
+console.log( 5 === 5", 5 === 5" ) //false
+console.log( 5 =="5", "5" == 5" ) //true
+console.log( 5 === 5", "5" === ) //false
+console.log( 5 === 5, 5 === 5 ) //true*/
+
+let num1 = Number(prompt( "Dame un numero"))
+let num2 = Number(prompt("Dame un segundo numero"))
+
+console.log("Su valor A es: " + num1)
+console.log("Su valor B es: " + num2)
+
+suma = num1 + num2
+console.log("Suma: " + suma)
+
+resta = num1 - num2
+console.log("Resta: " + resta)
+
+multiplicacion = num1 * num2
+console.log("Multiplicacion: " + multiplicacion)
+
+division = num1 / num2
+console.log("Division: " + division)
