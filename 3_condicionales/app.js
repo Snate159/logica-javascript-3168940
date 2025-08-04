6365c4e00d2098904aeda45c3fe678d17aec2cb9
Snate159 <santi.rrplay@gmail.com>
2025-08-04 13:31:55 -0500

    Primer ejercicio condicionales

---
+++ 3_condicionales/app.js
@@ -0,0 +1,16 @@
+let nombre = prompt("Dame tu nombre");
+let edad = Number(prompt("Dime tu edad"));
+
+if (edad >= 18) {
+    alert ("Puedes ingresar a la discoteca")
+}
+else {
+    alert ("No puedes ingresar a la discoteca")
+}
+
+if (nombre = "Mario") {
+    alert("Puedes ingresar a VIP")
+}
+else if (nombre = "Carlos") {
+    alert("Puedes ingresar a VIP")
+}
