958701b52fcfd5ac35371696ad5aa9dd183050f7
Snate159 <santi.rrplay@gmail.com>
2025-08-05 12:56:05 -0500

    Ejercicios terminados de condicionales

--- 3_condicionales/app.js
+++ 3_condicionales/app.js
@@ -1,4 +1,4 @@
-let nombre = prompt("Dame tu nombre");
+/* let nombre = prompt("Dame tu nombre");
 let edad = Number(prompt("Dime tu edad"));
 
 if (edad >= 18) {
@@ -8,9 +8,29 @@ else {
     alert ("No puedes ingresar a la discoteca")
 }
 
-if (nombre = "Mario") {
+if (nombre == "Mario") {
     alert("Puedes ingresar a VIP")
 }
-else if (nombre = "Carlos") {
+else if (nombre == "Carlos") {
     alert("Puedes ingresar a VIP")
+} else {
+    alert("No puedes ingresar a VIP")
+} */
+
+let estatura = Number(prompt("Cual es tu estatura en cm"))
+let velocidad = Number(prompt("Cual es tu velocidad en km/h"))
+let edad = Number(prompt("Cual es tu edad"))
+
+if (estatura >= 170 && velocidad >= 27) {
+    alert("Puedes ingresar a un equipo")
+}
+else {
+    alert("No cumples con los requerimientos")
+}
+
+if (edad >= 18) {
+    alert("Pasas a las divisiones mayores")
+}
+else {
+    alert("Pasas a las Divisiones menores")
 }
