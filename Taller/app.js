511d4be74d68b6f51e5644cbf4e3c15d533f4540
Snate159 <santi.rrplay@gmail.com>
2025-08-14 14:43:00 -0500

    Taller primer adelanto

---
+++ Taller/app.js
@@ -0,0 +1,51 @@
+/* // Ejercicio 1
+let num = Number(prompt("Introduce un numero"))
+if (num < 0) {
+    alert("Su numero es: " + num + " y es negativo")
+} else if (num >= 0){
+    alert ("Su numero es: " + num + " y es positivo")
+}*/
+
+
+/* //Ejercicio 2
+let num1 = Number(prompt("Introduce un numero"))
+let num2 = Number(prompt("Introduce un segundo numero"))
+if (num1 >= 0 && num2 >= 0) {
+    console.log("ambos numeros son positivos y son: " + num1 + " y " + num2)
+} else if (num1 >= 0) {
+    console.log(num1)
+} else if (num2 >= 0) {
+    console.log(num2)
+} else {
+    console.log("Wio wio wio, ningun numero es positivo")
+} */
+
+
+/* // Ejercicio 3
+let num1 = Number(prompt("Introduce un numero"))
+let num2 = Number(prompt("Introduce un segundo numero"))
+if (num1 == num2) {
+    console.log("son iguales: " + num1 + " * " + num2 + " = " + (num1*num2))
+} else if (num1 > num2) {
+    console.log("el primer numero es mayor: " + num1 + " - " + num2 + " = " + (num1-num2))
+} else {
+    console.log("el primero es un numero menor: " + num1 + " + " + num2 + " = " + (num1+num2))
+} */
+
+
+//Ejercicio 4
+/* let producto = 50000
+let dinero = Number(prompt("El producto cuesta 50000$, con cuanto vas a pagar?"))
+if (dinero < producto) {
+    let resta = producto-dinero
+    console.log("Te falta " + resta + " Para obtener el producto")
+} else if (dinero == producto) {
+    console.log("Gracias, feliz dia, disfruta tu producto, vuelva pronto (:")
+} else {
+    let devuelta = dinero-producto
+    console.log("Te devolvemos " + devuelta + " Disfruta tu producto")
+} */
+
+
+//Ejercicio 5
+ 
