17902faee1a522d8ded333eace50ce960b414694
Snate159 <santi.rrplay@gmail.com>
2025-09-04 13:07:00 -0500

    Dom parte 2

--- Dom/app.js
+++ Dom/app.js
@@ -3,6 +3,7 @@
 const pantalla = document.querySelector(".pantalla")
 const btnMenos = document.querySelector(".btn-menos")
 const btnMas = document.querySelector(".btn-mas")
+const body = document.body;
 let counter = 0
 
 //2. Funcion
@@ -11,15 +12,21 @@ function incrementar () {
     if (counter >= 10) {
         pantalla.style.color = "red"
     }
+    if (counter === 30) {
+        body.style.backgroundColor = "green"
+    }
     pantalla.textContent = counter
 }
 
 function decrementar () {
-    counter--
-    if (counter < 10) {
-        pantalla.style.color = "black"
-    } 
-    pantalla.textContent = counter
+    
+    if (counter > 0) {
+        counter--
+        if (counter < 10) {
+            pantalla.style.color = "black"
+        }
+        pantalla.textContent = counter
+    }
 }
 
 
