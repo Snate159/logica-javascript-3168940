ecda2139d35606717f5bace48a720d8e8cafefc3
Snate159 <santi.rrplay@gmail.com>
2025-09-10 13:43:32 -0500

    Dom formulario

---
+++ Dom_2/app.js
@@ -0,0 +1,28 @@
+
+const formulario = document.querySelector(".form")
+const loginContainer = document.querySelector(".login")
+const inputNombre = document.querySelector(".inputNombre")
+const listaColores = document.querySelector("#color")
+const userPage = document.querySelector(".userPage")
+const imgEquipo = document.querySelector(".img-equipo")
+const textSaludo = document.querySelector(".text-saludo")
+const textEquipo = document.querySelector(".text-equipo")
+
+const enviarData = (e) => {
+  const equipo = document.querySelector('input[name="equipo"]:checked')
+
+  e.preventDefault() 
+  const userInfo = {
+    nombre: inputNombre.value,
+    color: listaColores.value,
+    equipo: equipo.value
+  }
+  console.log(userInfo)
+  document.body.style.background = userInfo.color
+  userPage.style.display = "block"
+  loginContainer.style.display = "none"
+  //
+  //
+}
+
+formulario.addEventListener("submit",enviarData)
