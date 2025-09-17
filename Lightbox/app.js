d31e0eb094f520db1a90a133b7308f4ad806d809
Snate159 <santi.rrplay@gmail.com>
2025-09-17 13:49:13 -0500

    Lightbox incompleto

---
+++ Lightbox/app.js
@@ -0,0 +1,22 @@
+const flotante = document.querySelector("#flotante")
+const cuadro = document.querySelector("#cuadro")
+const equis = document.querySelector("#equis")
+const sig = document.querySelector("#sig")
+const ant = document.querySelector("#ant")
+const imgprincipal = document.querySelector("#imgprincipal")
+const fath = document.querySelector("#fath")
+
+function mostrarLightbox (){
+
+
+}
+function ocultarLightbox() {
+
+}
+
+function siguiente (){
+
+}
+function anterior () {
+    
+}
