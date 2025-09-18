3ec62da4471a2171f86b15e41320fd34c349c09d
Snate159 <santi.rrplay@gmail.com>
2025-09-18 13:23:37 -0500

    Add files via upload

---
+++ Teoria Scroll/app_01.js
@@ -0,0 +1,16 @@
+const main = document.querySelector("#main")
+const valorVertical = document.querySelector(".valor-vertical")
+const valorHorizontal = document.querySelector(".valor-horizontal")
+const pelota = document.querySelector (".pelota")
+
+function verScroll () {
+let top = main.scrollTop
+let left = main.scrollLeft
+valorVertical.textContent = `V: ${top}`
+valorHorizontal.textContent = `H: ${left}`
+pelota.style.top = `${top}px`
+pelota.style.left = `${left}px`
+pelota.style.transform = `scale(${(top/30)})`
+}
+
+main.addEventListener("scroll",verScroll)
