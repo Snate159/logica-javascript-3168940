ee0bc46fe3e01909cbca907f705171743aff820f
Snate159 <santi.rrplay@gmail.com>
2025-09-25 13:52:31 -0500

    Dom Taller

--- Dom_Taller/app.js
+++ Dom_Taller/app.js
@@ -2,6 +2,16 @@ const mensaje = document.querySelector("#mensaje")
 const boton = document.querySelector(".Color")
 const mensaje2 = document.querySelector("#mensaje2")
 const btnMensaje = document.querySelector(".btnMensaje")
+const input = document.querySelector("#input")
+const btnMostrar = document.querySelector(".btnMostrar")
+const resultado = document.querySelector("#resultado")
+const rojo = document.querySelector(".rojo")
+const verde = document.querySelector(".verde")
+const amarillo = document.querySelector(".amarillo")
+const formulario = document.querySelector("#formulario")
+const resultado2 = document.querySelector("#resultado2")
+
+
 
 
 mensaje.textContent = "Hola, este texto fue modificado con JavaScript"
@@ -20,4 +30,28 @@ btnMensaje.addEventListener("click", function () {
     } else {
         mensaje2.style.display = "none"
     }
+})
+
+btnMostrar.addEventListener("click", function () {
+    const texto = input.value
+    resultado.textContent = texto
+})
+
+rojo.addEventListener("click", function () {
+    document.body.style.backgroundColor = "red"
+})
+
+verde.addEventListener("click", function () {
+    document.body.style.backgroundColor = "green"
+})
+
+amarillo.addEventListener("click", function () {
+    document.body.style.backgroundColor = "yellow"
+})
+
+formulario.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const nombre = document.querySelector("#nombre").value
+    const edad = document.querySelector("#edad").value
+    resultado2.textContent = `Hola ${nombre}, tienes ${edad} años.`
 })
